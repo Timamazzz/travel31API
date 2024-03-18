d504a67976180bf7a3cdf5eae1d53b263008aefb
Timama <timforworking@mail.ru>
2024-03-19 02:18:49 +0300

    push

--- applications_app/urls.py
+++ applications_app/urls.py
@@ -4,10 +4,10 @@ from rest_framework.routers import DefaultRouter
 from applications_app.views import ApplicantViewSet, MunicipalityViewSet, SchoolViewSet, ApplicationViewSet
 
 router = DefaultRouter()
-router.register(r'applicants/', ApplicantViewSet)
-router.register(r'municipalities/', MunicipalityViewSet)
-router.register(r'schools/', SchoolViewSet)
-router.register(r'applications/', ApplicationViewSet)
+router.register(r'applicants', ApplicantViewSet)
+router.register(r'municipalities', MunicipalityViewSet)
+router.register(r'schools', SchoolViewSet)
+router.register(r'applications', ApplicationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
