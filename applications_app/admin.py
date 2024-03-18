b5c20ebb17cf3444fce53e7464012a3d55036db4
Timama <timforworking@mail.ru>
2024-03-19 01:10:32 +0300

    push

--- applications_app/admin.py
+++ applications_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Municipality, School, Applicant, Application
 
-# Register your models here.
+
+@admin.register(Municipality)
+class MunicipalityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'municipality']
+
+
+@admin.register(Applicant)
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'telegram_id', 'phone_number']
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['applicant', 'municipality', 'school', 'child_full_name', 'child_gender', 'child_age',
+                    'received_offer', 'duration']
